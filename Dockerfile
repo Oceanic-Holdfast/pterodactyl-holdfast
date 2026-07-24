5de33e42e0dcef7e10ec109be4c16a20e3cc2859
Lucas Berry <luckinber@gmail.com>
2026-07-24 12:41:26 +1000

    Moved to steamcmd docker image and upated entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,39 @@
-# ----------------------------------
-# Environment: ubuntu
-# Minimum Panel Version: 0.7.X
-# ----------------------------------
-FROM  ubuntu:18.04
+FROM steamcmd/steamcmd:ubuntu-24
 
-LABEL author="Ankit Patel" maintainer="ankit@bmghosting.com"
+LABEL author="Lucas Berry" maintainer="lucas@luckinber.com"
 
-ENV   DEBIAN_FRONTEND noninteractive
+# Install runtime dependencies commonly required by Steam/Unity dedicated servers.
+RUN apt-get update \
+	&& apt full-upgrade -y \
+	&& apt-get install -y --no-install-recommends \
+		ca-certificates \
+		curl \
+		file \
+		iproute2 \
+		libc6-i386 \
+		lib32gcc-s1 \
+		lib32stdc++6 \
+		libsdl2-2.0-0 \
+		tzdata \
+	&& rm -rf /var/lib/apt/lists/*
 
-## add container user
-RUN   useradd -m -d /home/container -s /bin/bash container
-
-## update base packages
-RUN   apt update \
- &&   apt upgrade -y
-
-## install dependencies
-RUN   apt install -y gcc g++ libgcc1 lib32gcc1 gdb libc6 libstdc++6 git wget curl tar zip unzip binutils xz-utils liblzo2-2 bzip2 zlib1g iproute2 net-tools netcat telnet libatomic1 libsdl1.2debian libsdl2-2.0-0 \
-      libfontconfig libicu60 libiculx60 icu-devtools libunwind8 libssl1.0.0 libssl1.0-dev sqlite3 libsqlite3-dev libmariadbclient-dev libduktape202 libzip4 locales ffmpeg apt-transport-https 
-
-## Steamclient.so Link
-RUN ln -s "/home/container/steamcmd/linux64/steamclient.so" "/usr/lib/x86_64-linux-gnu/steamclient.so" 
-
-## configure locale
-RUN   update-locale lang=en_US.UTF-8 \
- &&   dpkg-reconfigure --frontend noninteractive locales
-
+# Create the Pterodactyl container user and home.
+RUN useradd -d /home/container -m container
 USER container
-ENV  USER=container HOME=/home/container
-
+ENV USER=container HOME=/home/container
 WORKDIR /home/container
 
-COPY  ./entrypoint.sh /entrypoint.sh
-CMD   ["/bin/bash", "/entrypoint.sh"]
+ENV STEAMAPPID=1424230
+ENV STEAMAPP=holdfastnaw
+ENV STEAMAPPDIR="${HOME}/${STEAMAPP}-dedicated"
+
+ENV CONFIG_DIR="${STEAMAPPDIR}/configs"
+ENV ADMIN_DIR="${CONFIG_DIR}/admin"
+ENV LOG_DIR="${STEAMAPPDIR}/logs"
+ENV WORKSHOP_DIR="${STEAMAPPDIR}/workshop"
+RUN mkdir -p "${CONFIG_DIR}" "${ADMIN_DIR}" "${LOG_DIR}" "${WORKSHOP_DIR}"
+
+COPY --chown=container:container ./entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/bin/bash", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 cd /home/container
 
-# Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+# Install/Update game using steamcmd
+steamcmd \
+	+force_install_dir ${STEAMAPPDIR} \
+	+login anonymous \
+	+app_update ${STEAMAPPID} validate \
+	+quit
 
-# Modify the configuration variables using sed
-pushd holdfastnaw-dedicated/configs/
-if [ -f "$SERVER_CONFIG_PATH" ]; then
-	echo "Found configuration file - replacing variables"
-	sed -i "s/^server_name .*/server_name $(printf '%s\n' "$SERVER_NAME" | sed -e 's/[\/&]/\\&/g')/g" "$SERVER_CONFIG_PATH"
-	sed -i "s/^maximum_players .*/maximum_players $(printf '%s\n' "$PLAYERS" | sed -e 's/[\/&]/\\&/g')/g" "$SERVER_CONFIG_PATH"
-	sed -i "s/^server_welcome_message .*/server_welcome_message $(printf '%s\n' "$MOTD" | sed -e 's/[\/&]/\\&/g')/g" "$SERVER_CONFIG_PATH"
-	sed -i "s/^server_region .*/server_region $(printf '%s\n' "$REGION" | sed -e 's/[\/&]/\\&/g')/g" "$SERVER_CONFIG_PATH"
-	sed -i "s/^server_admin_password .*/server_admin_password $(printf '%s\n' "$ADMIN_PASS" | sed -e 's/[\/&]/\\&/g')/g" "$SERVER_CONFIG_PATH"
-	sed -i "s/^#\{0,1\}server_password .*/server_password $(printf '%s\n' "$SERVER_PASS" | sed -e 's/[\/&]/\\&/g')/g" "$SERVER_CONFIG_PATH"
-	sed -i "s/^server_port .*/server_port $(printf '%s\n' "$SERVER_PORT" | sed -e 's/[\/&]/\\&/g')/g" "$SERVER_CONFIG_PATH"
-	sed -i "s/^steam_communications_port .*/steam_communications_port $(printf '%s\n' "$SERVER_COMM_PORT" | sed -e 's/[\/&]/\\&/g')/g" "$SERVER_CONFIG_PATH"
-	sed -i "s/^steam_query_port .*/steam_query_port $(printf '%s\n' "$SERVER_QUERY_PORT" | sed -e 's/[\/&]/\\&/g')/g" "$SERVER_CONFIG_PATH"
-else
-	echo "Configuration file not found: $SERVER_CONFIG_PATH"
+if [ ! -f "$CONFIG_FILE" ]; then
+  cp "$STEAMAPPDIR/serverconfig_default.txt" "$CONFIG_DIR/serverconfig_default.txt"
+  CONFIG_FILE="serverconfig_default.txt"
 fi
-popd
 
-# Replace Startup Variables
-MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
-START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
-echo -e ":/home/container$ ${START_COMMAND}"
-
-# Run the Server
-eval ${MODIFIED_STARTUP}
+"$STEAMAPPDIR/Holdfast NaW" \
+-startserver \
+-serverheadless \
+-batchmode \
+-nographics \
+-screen-width 640 \
+-screen-height 480 \
+-screen-quality "Fastest" \
+-serverConfigFilePath "$CONFIG_DIR/$CONFIG_FILE" \
+-bannedPlayersFilePath "$ADMIN_DIR/bannedplayers.txt" \
+-bannedMachinesFilePath "$ADMIN_DIR/bannedmachines.txt" \
+-mutedVoipPlayersFilePath "$ADMIN_DIR/muteplayersvoip.txt" \
+-micSpammersPlayersFilePath "$ADMIN_DIR/micspammers.txt" \
+-mutedChatPlayersFilePath "$ADMIN_DIR/muteplayerschat.txt" \
+-serverAdminsFilePath "$ADMIN_DIR/serverAdmins.txt" \
+-logFile "$LOG_DIR/outputlog.txt" \
+-logArchivesDirectory "$LOG_DIR/logs_archive/" \
+-adminCommandsLogFilePath "$LOG_DIR/adminactions.txt" \
+-playersLogFilePath "$LOG_DIR/playerlogin.txt" \
+-scoreboardLogFilePath "$LOG_DIR/scorelog.txt" \
+-chatLogFilePath "$LOG_DIR/chatlog.txt" \
+-vacLogFilePath "$LOG_DIR/vaclog.txt" \
+-workshopDataPath "$WORKSHOP_DIR"
